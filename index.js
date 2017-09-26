dfa2e13ef0e72a53126653db9707ba7ced9b3af9
Lance Hartung <hartung@cs.wisc.edu>
2017-09-26 10:36:12 -0500

    Updated paradrop.yaml to match new developments.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', function(req, res) {
   res.send('Hello!');
 });
 
-app.listen(80, function() {
-  console.log('Listening on port 80.');
+app.listen(3000, function() {
+  console.log('Listening on port 3000.');
 });
