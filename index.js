f103e14fcac850808303015b3878b58fc477f68d
Ubuntu <ubuntu@paradrop-tutorial>
2017-10-15 20:19:54 +0000

    Added mu messages to the paradrop hello world fn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello!');
+  res.send('Hello,'+ req.headers['user-agent']+'!');
+  res.send('<h1>Hello, World!</h1><p>From '+process.env.PARADROP_CHUTE_NAME+'</p>')
 });
 
 app.listen(3000, function() {
